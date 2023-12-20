67eaad7ad77fbe266f19e4deb77db8a2961c33a1
Sebastian Vasquez <sebitas0489@gmail.com>
2023-12-19 22:29:21 -0500

    Funcionalidad boton RESET.

--- js/app.js
+++ js/app.js
@@ -14,11 +14,23 @@ document.addEventListener('DOMContentLoaded', function() {
   const inputMensaje = document.querySelector('#mensaje');
   const formulario = document.querySelector('#formulario');
   const btnSubmit = document.querySelector('#formulario button[type="submit"');
-
+  const btnReset = document.querySelector('#formulario button[type="reset"');
+  
   // Asignamos eventos. 
   inputEmail.addEventListener('input', validar);
   inputAsunto.addEventListener('input', validar);
   inputMensaje.addEventListener('input', validar);
+
+  btnReset.addEventListener('click', function(e) {
+    e.preventDefault();
+    // Reiniciar el objeto
+    email.email = '';
+    email.asunto = '';
+    email.mensaje = '';
+
+    formulario.reset();
+    comprobarEmail();
+  })
 
   //Creamos funciones para su validación.
   function validar(e){
