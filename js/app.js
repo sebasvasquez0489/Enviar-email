d4d0500d29dd34acc208203ecba69f06153940ac
Sebastian Vasquez <sebitas0489@gmail.com>
2023-12-19 22:22:02 -0500

    Activación de botón ENVIAR al completar los campos.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,46 @@
 // Utilizamos el DOMContentLoaded para que se ejecute el codigo cuando todo el HTML haya descargado.
 document.addEventListener('DOMContentLoaded', function() {
 
+  //Creamos un Objeto para la validacion de los campos y activacion del boyon ENVIAR.
+  const email = {
+    email: '',
+    asunto: '',
+    mensaje: ''
+  }
+
   // Seleccionamos los elementos de la interfaz.
   const inputEmail = document.querySelector('#email');
   const inputAsunto = document.querySelector('#asunto');
   const inputMensaje = document.querySelector('#mensaje');
   const formulario = document.querySelector('#formulario');
+  const btnSubmit = document.querySelector('#formulario button[type="submit"');
 
   // Asignamos eventos. 
-  inputEmail.addEventListener('blur', validar);
-  inputAsunto.addEventListener('blur', validar);
-  inputMensaje.addEventListener('blur', validar);
+  inputEmail.addEventListener('input', validar);
+  inputAsunto.addEventListener('input', validar);
+  inputMensaje.addEventListener('input', validar);
 
   //Creamos funciones para su validación.
   function validar(e){
     if (e.target.value.trim() === '') {
       mostrarAlerta(`El campo ${e.target.id}  es obligatorio`, e.target.parentElement);
+      email[e.target.name] = '';
+      comprobarEmail();
       return;
     }
     if(e.target.id === 'email' && !validarEmail(e.target.value)) {
       mostrarAlerta('El email no es valido', e.target.parentElement);
+      email[e.target.name] = '';
+      comprobarEmail();
       return;
     };
     limpiarAlerta(e.target.parentElement);
+
+    // Asignar valores al Objeto
+    email[e.target.name] = e.target.value.trim().toLowerCase();
+    console.log(email);
+    // Comprobar el objeto email
+    comprobarEmail();
   }
 
   function mostrarAlerta(mensaje, referencia) {
@@ -51,4 +69,13 @@ document.addEventListener('DOMContentLoaded', function() {
     return resultado;
   }
 
+  function comprobarEmail() {
+    if(Object.values(email).includes('')) {
+      btnSubmit.classList.add('opacity-50');
+      btnSubmit.disabled = true;
+      return
+    }
+      btnSubmit.classList.remove('opacity-50');
+      btnSubmit.disabled = false;
+  }
 });
